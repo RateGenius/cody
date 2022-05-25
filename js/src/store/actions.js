8f5142c3b11654e1243462c769f98bde1cb2530b
tdrdimov-dvlpr <todor@simplertrading.com>
2022-05-25 01:30:38 -0500

    fetch data & populate data table

--- js/src/store/actions.js
+++ js/src/store/actions.js
@@ -3,5 +3,14 @@ import axios from 'axios';
 export default {
   loadLoanApplications: async ({ commit }) => {
     // TODO: make a GET request
+    const baseURL = 'https://6271819ac455a64564b41998.mockapi.io';
+    try {
+      const data = await axios.get(`${baseURL}/api/v3/applications`);
+      if (data.data.status === 'success') {
+        commit('SET_APPLICATIONS', data.data.data.applications);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 };
